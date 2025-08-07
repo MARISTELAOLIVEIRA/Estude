292a13ac46ece708361b7d98954df9882003dee8
Maristela <maristela33781197@edu.df.senac.br>
2025-08-07 13:43:49 +0000

    Configurar templates, static files e internacionalização
    - Configurar diretório de templates
    - Adicionar configurações para arquivos estáticos
    - Definir locale para pt-br e timezone America/Sao_Paulo
    - Configurar tags de mensagens Bootstrap
    - Adicionar app usuarios aos INSTALLED_APPS

---
+++ flashcard/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Categoria, Flashcard
+
+admin.site.register(Categoria)
+admin.site.register(Flashcard)
+# This code registers the Categoria and Flashcard models with the Django admin site,
